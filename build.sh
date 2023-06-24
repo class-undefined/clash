cf49a9681b13363df0fa2b932508b41ccaa44ccd
野漫横江 <1399226280@qq.com>
2023-06-24 11:37:12 +0800

    fix: image name

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ mkdir res
 wget -O res/Centos-7.repo http://mirrors.aliyun.com/repo/Centos-7.repo 
 wget -O res/clash-linux-amd64-v1.16.gz https://github.com/Dreamacro/clash/releases/download/v1.16.0/clash-linux-amd64-v1.16.0.gz
 wget -O res/Country.mmdb https://github.com/Dreamacro/maxmind-geoip/releases/latest/download/Country.mmdb
-docker build -t clash --build-arg subscribe=$subscribe .
+docker build -t clash-proxy --build-arg subscribe=$subscribe .
